188492319955536f3f12c670fdddf01a46be8dbf
Cristiano Solarino <cristiano.solarino@brightminded.com>
2014-03-23 16:42:27 +0000

    now using separate connections for producing and consuming

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 (function(){
   exports.amqp = {
-    url: 'amqp://guest:guest@localhost:5672',
+    url: {
+      'produce': process.env.RABBITMQ_BIGWIG_TX_URL || 'amqp://guest:guest@localhost:5672',
+      'consume': process.env.RABBITMQ_BIGWIG_RX_URL || 'amqp://guest:guest@localhost:5672',
+    },
     testurl: 'amqp://guest:guest@localhost:5672',
     reqexchange: 'clogger.api.x.req',
     resexchange: 'clogger.api.x.res',
